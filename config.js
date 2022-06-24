79bffe6d31545514ed5333687da802447a762809
gkhiem123 <108112978+gkhiem123@users.noreply.github.com>
2022-06-24 22:02:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const CONFIG = {
     desc: 'anh yêu em bé của anh nhiều như số ngày em bé đến tháng',
     btnYes: 'cũng là "vâng ạ" nhưng mà "vâng ạ" này yêu anh nhiều hơn',
     btnNo: 'vâng ạ',
-    question: 'em bé muốn quà gì',
+    question: 'em bé muốn quà gì?',
     btnReply: 'send.',
     reply: 'em muốn ngiu',
     mess: 'Cảm ơn em bé đã luôn bên anh, ngay cả khi anh k còn múi. Mong rằng em bé sẽ luôn vui vẻ, k được khóc nhè nữa đâu nha',
